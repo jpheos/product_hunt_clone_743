c799cbe0f6dafbb05a6d186de0b64c4547546759
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2021-11-26 10:18:54 +0100

    run unit and system tests on GHA

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # def full_name
+  #   return last_name.capitalize if first_name.blank?
+
+  #   "#{first_name.capitalize} #{last_name.capitalize}"
+  # end
+
+  def full_name
+    if first_name.blank?
+      last_name.capitalize
+    else
+      "#{first_name.capitalize} #{last_name.capitalize}"
+    end
+  end
+
 end
